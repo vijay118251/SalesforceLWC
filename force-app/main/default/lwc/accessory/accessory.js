9c93d0836ad8fb5f2dff80227adb5a5fe29d6dd2
vijay118251 <vijay118251@gmail.com>
2025-03-22 18:01:15 +0530

    LWC Code

---
+++ force-app/main/default/lwc/accessory/accessory.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import PhoneCase from '@salesforce/resourceUrl/PhoneCase';
+export default class Accessory extends LightningElement {
+    imageUrl = PhoneCase;
+}
